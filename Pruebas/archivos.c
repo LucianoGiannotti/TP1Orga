f767e55b16419cc846acbc42c61f4d24e20c8beb
Santiago-Pinto <pinto.santiago.augusto@gmail.com>
2017-10-08 01:33:14 +0200

    Add files via upload
    Ver en el archivo resumen para saber que hay en esta carpeta. En resumen hicimos la funcion getchar y jugamos un poco con el pasaje de control entre c  y mips.

--- Pruebas/archivos.c
+++ Pruebas/archivos.c
@@ -5,9 +5,23 @@
 #include <sys/types.h>
 #include "mymalloc.h"
 
-#define BUFF_SIZE 10
+#define INPUT_BUFF_SIZE 10
+#define OUTPUT_BUFF_SIZE 5
+extern void getch(char* src, char** dest, size_t indice);
+
 /**Funcion de mips a la cual le cedo el control**/
-extern void mips_foo(int fdi, int fdo,size_t buff_size);
+void palindrome(int ifd, size_t ibytes, int ofd, size_t obytes){
+	unsigned int indice = 1;
+	//Cargo el buffer de entrada
+	char* buffer = (char*)mymalloc(ibytes);
+	read(ifd,buffer,ibytes);
+	//Paso 1 byte a un buffer interno
+	char* buffer_interno = (char*)mymalloc(20);
+	//pasa un byte desde buffer a buffer_interno
+	getch(buffer, &buffer_interno, indice);
+	++indice;
+	write(ofd,buffer_interno,1);
+}
 
 int main(int argc, char* argv[]){
 	//Abro el file y obtengo su fd
@@ -15,13 +29,13 @@ int main(int argc, char* argv[]){
 	FILE* output = fopen("output.txt","w+");
 	int fdi = fileno(archivo);
 	int fdo = fileno(output);
-	//Le cedo el control a mips, esta funcion simplemente lee BUFF_SIZE caracteres
-	//del archivo cuyo file descriptor es fdi, los coloca en un buffer y escribe sobre un 
-	//archivo de salida output.txt
-	mips_foo(fdi,fdo,BUFF_SIZE);
-	
+	//palindrome por ahora simplemente lee INPUT_BUFF_SIZE caracteres del archivo de entrad
+	//prueba.txt, los carga en un buffer, y hace un llamado a una funcion getch que carga
+	//en un buffer interno de a 1 byte
+	palindrome(fdi,INPUT_BUFF_SIZE,fdo,OUTPUT_BUFF_SIZE);
 	//Al finalizar cierro los archivos
 	fclose(archivo);
 	fclose(output);
 	return 0;
 }
+
