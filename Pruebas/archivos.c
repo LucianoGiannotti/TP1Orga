2ef2a99715b95084f794178cb72d6f49b9b1964f
Unknown <fabrizio.cozza@gmail.com>
2017-10-08 20:50:43 -0300

    "modelo" buffer i/o MIPS en func palindromo + realloc casero en C
    falta: funciones invertir y escapicua, pasar realloc a MIPS y meterlo en el while de la funcion palindromo

--- Pruebas/archivos.c
+++ Pruebas/archivos.c
@@ -5,34 +5,45 @@
 #include <sys/types.h>
 #include "mymalloc.h"
 
-#define INPUT_BUFF_SIZE 10
+#define INPUT_BUFF_SIZE 5
 #define OUTPUT_BUFF_SIZE 5
 extern void putch(char* src, char** dest, size_t indice);
 extern char getch(char* src,size_t indice);
-/*char getch(char* src,size_t indice){
-	char res = (char) src[indice];
-	return res;
-} */
-
-
 /**Funcion de mips a la cual le cedo el control**/
-void palindrome(int ifd, size_t ibytes, int ofd, size_t obytes){
-	unsigned int indice = 1;
-	//Cargo el buffer de entrada
-	char* ibuffer = (char*)mymalloc(ibytes);
-	char* obuffer = (char*)mymalloc(obytes);
-	read(ifd,ibuffer,ibytes);
-	//Paso 1 byte a un buffer interno
-	char* buffer_interno = (char*)mymalloc(20);
-	//pasa un byte desde ibuffer a buffer_interno
-	putch(ibuffer, &buffer_interno, indice);
-	//paso un byte desde buffer_interno a obuffer
-	putch(buffer_interno, &obuffer, indice);
-	char a = 's';
-	a = getch(ibuffer,indice); //devuelve el caracter del indice indicado
-	printf("%c\n",a);
-	++indice;
-	write(ofd,obuffer,1);
+extern void palindrome(int ifd, size_t ibytes, int ofd, size_t obytes);
+
+char* invertir(const char* cadena, const size_t tamanio){
+    char* inverso = (char*)calloc(tamanio + 1, sizeof(char));
+    if (!inverso){
+       return NULL;
+    }
+    for (unsigned int i = 1; i<tamanio + 1; ++i){
+         strncat(inverso,cadena + (tamanio-i), 1);
+    }
+    return inverso;
+}
+
+
+char* initBufferInterno(size_t size){
+	char* buffer = (char*)mymalloc(size);
+	memset(buffer, size,0);
+}
+
+void llenarBufferInterno(char caracter, char** buffer_interno){
+     if (strlen(buffer_interno) == sizeof(buffer_interno)){
+	/**Version casera del realloc**/
+	char* nuevo_buffer = (char*)mymalloc(sizeof(*buffer_interno)*2);
+	memset(nuevo_buffer,0, sizeof(nuevo_buffer));
+	strncpy(nuevo_buffer, *buffer_interno, sizeof(*buffer_interno));
+	myfree(buffer_interno);
+	buffer_interno = &nuevo_buffer;
+     }	     
+     if (caracter == '\0'){
+	/**Se termino la palabra, ya puedo invertirla**/
+     } else {
+	*buffer_interno[strlen(*buffer_interno)] =  caracter;
+     }
+     
 }
 
 int main(int argc, char* argv[]){
